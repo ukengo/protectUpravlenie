5e08e727807f64ae2b86ce54dcd1ea85f26b0dc4
ukengo <office@friedman.com.ua>
2022-06-21 19:07:31 +0300

    optimized work algorithm

--- Код.js
+++ Код.js
@@ -1,4 +1,3 @@
-
 function protect() {
   // получаем таблицу
   const url = 'https://docs.google.com/spreadsheets/d/1xdJVfecdUCgtF_SHiC_IHbl8FP0Agl3slV_osyj6kxo/edit#gid=0';
@@ -7,52 +6,52 @@ function protect() {
   const sheet = ss.getSheetByName(sheetName);
 
   // сбрасываем все ранее установленные защиты
-  unPropert(sheet);
+  unProtect(sheet);
 
   // получаем весь массив таблицы
   const valuesStart = sheet.getDataRange().getValues();
-  
-  ////////////////////////////
-  // берем в работу первую часть заполненного массива (стартовая установка)
-  let values = valuesStart;
-  let indOf = values.map(x => x[0]).indexOf(''); // находим индекс последнего заполненного значения в блоке
-  let row = indOf; // переменная с номером последней строки диапазона
-  // защищаем первый диапазон  
-  protectsss(sheet, 1, 1, indOf, values)
-  ////////////////////////////
 
-  ///////////////////////////////////
-  // берем второй диапазон после первого
-  values = sheet.getRange(row + 1, 1, valuesStart.length, valuesStart[0].length).getValues()
-  // пропускаем пустоты
-  row = noBlank(values, row)
-  // берем второй диапазон после первого уже без пустот
-  values = sheet.getRange(row, 1, valuesStart.length, valuesStart[0].length).getValues()
-  indOf = values.map(x => x[0]).indexOf('')// находим индекс последнего заполненного значения в блоке
-  // защищаем 
-  protectsss(sheet, row, 1, indOf, values)
-  /////////////////////////////////
+  // берем в работу первую часть заполненного массива (стартовая установка)
+
+  // находим индекс последнего заполненного значения в блоке
+  let indexEndnoBlank = valuesStart.map(x => x[0]).indexOf('');
+  // защищаем первый диапазон  
+  protects(sheet, 1, 1, indexEndnoBlank, valuesStart[0].length)
+
+
+  let indexStart = indexEndnoBlank + 1
 
   for (let i = 0; i < 10000; i++) {
-    row = row + indOf;
-    values = sheet.getRange(row + 1, 1, valuesStart.length, valuesStart[0].length).getValues();
-    // пропускаем пустоты
-    row = noBlank(values, row);
-    values = sheet.getRange(row, 1, valuesStart.length, valuesStart[0].length).getValues();
-    indOf = values.map(x => x[0]).indexOf('');
-    if (!indOf) return;
-    protectsss(sheet, row, 1, indOf, values);
+    // берем второй диапазон после первого
+    values = sheet.getRange(indexStart, 1, valuesStart.length, valuesStart[0].length).getValues()
+
+    // находим индекс первого элемента следующего блока с данными (пропускаем пустоты)
+    indexStartnoBlank = noBlank(values, indexStart)
+    
+    // берем второй диапазон после первого уже без пустот
+    values = sheet.getRange(indexStartnoBlank, 1, valuesStart.length, valuesStart[0].length).getValues()
+
+    // находим индекс последнего заполненного значения в блоке
+    indexEndBlock = values.map(x => x[0]).indexOf('')
+    
+    if (!indexEndBlock) return;
+
+    // защищаем 
+    protects(sheet, indexStartnoBlank, 1, indexEndBlock, values[0].length)
+
+    // находим начало следующего блока
+    indexStart = indexStartnoBlank + indexEndBlock + 1
   }
 }
 
 // пропускаем пустоты
 function noBlank(values, row) {
   const index = values.map(x => x[0]).findIndex(val => val != '')
-  return row + index + 1
+  return row + index
 }
 
-function protectsss(sheet, cell, col, indOf, values) {
-  const protection = sheet.getRange(cell, col, indOf, values[0].length).protect().setDescription('Защищено. Обратитесь к администратору');
+function protects(sheet, row, col, rows, cols) {
+  const protection = sheet.getRange(row, col, rows, cols).protect().setDescription('Защищено. Обратитесь к администратору');
   const me = Session.getEffectiveUser();
   protection.addEditor(me);
   protection.removeEditors(protection.getEditors());
@@ -61,7 +60,7 @@ function protectsss(sheet, cell, col, indOf, values) {
   }
 }
 
-function unPropert(ss) {
+function unProtect(ss) {
   const protections = ss.getProtections(SpreadsheetApp.ProtectionType.RANGE);
   for (let i = 0; i < protections.length; i++) {
     const protection = protections[i];
