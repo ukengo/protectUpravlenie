685457d8b5e8de677efdc3deeb2e21c137790228
ukengo <office@friedman.com.ua>
2023-02-09 12:00:49 +0200

    edit const protection

--- Код.js
+++ Код.js
@@ -16,48 +16,49 @@ function protect() {
   // находим индекс последнего заполненного значения в блоке
   let indexEndnoBlank = valuesStart.map(x => x[0]).indexOf('');
   // защищаем первый диапазон  
-  protects(sheet, 1, 1, indexEndnoBlank, valuesStart[0].length)
+  protects(sheet, 1, 1, indexEndnoBlank, valuesStart[0].length);
 
 
-  let indexStart = indexEndnoBlank + 1
+  let indexStart = indexEndnoBlank + 1;
 
   for (let i = 0; i < 10000; i++) {
     // берем второй диапазон после первого
-    values = sheet.getRange(indexStart, 1, valuesStart.length, valuesStart[0].length).getValues()
+    values = sheet.getRange(indexStart, 1, valuesStart.length, valuesStart[0].length).getValues();
 
     // находим индекс первого элемента следующего блока с данными (пропускаем пустоты)
-    indexStartnoBlank = noBlank(values, indexStart)
+    indexStartnoBlank = noBlank(values, indexStart);
     
     // берем второй диапазон после первого уже без пустот
-    values = sheet.getRange(indexStartnoBlank, 1, valuesStart.length, valuesStart[0].length).getValues()
+    values = sheet.getRange(indexStartnoBlank, 1, valuesStart.length, valuesStart[0].length).getValues();
 
     // находим индекс последнего заполненного значения в блоке
-    indexEndBlock = values.map(x => x[0]).indexOf('')
+    indexEndBlock = values.map(x => x[0]).indexOf('');
     
     if (!indexEndBlock) return;
 
     // защищаем 
-    protects(sheet, indexStartnoBlank, 1, indexEndBlock, values[0].length)
+    protects(sheet, indexStartnoBlank, 1, indexEndBlock, values[0].length);
 
     // находим начало следующего блока
-    indexStart = indexStartnoBlank + indexEndBlock + 1
+    indexStart = indexStartnoBlank + indexEndBlock + 1;
   }
 }
 
 // пропускаем пустоты
 function noBlank(values, row) {
-  const index = values.map(x => x[0]).findIndex(val => val != '')
-  return row + index
+  const index = values.map(x => x[0]).findIndex(val => val != '');
+  return row + index;
 }
 
 function protects(sheet, row, col, rows, cols) {
-  const protection = sheet.getRange(row, col, rows, cols).protect().setDescription('Защищено. Обратитесь к администратору');
+  const protection = sheet.getRange(row, col, rows, cols).protect().setDescription('Проект закритий. Для внесення правок зверныться до Віталія');
   const me = Session.getEffectiveUser();
   protection.addEditor(me);
   protection.removeEditors(protection.getEditors());
   if (protection.canDomainEdit()) {
     protection.setDomainEdit(false);
   }
+  //protection.addEditor('ukengo2022@neural-myth-360416.iam.gserviceaccount.com');
 }
 
 function unProtect(ss) {
